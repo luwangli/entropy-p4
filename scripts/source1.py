a9cd8c1806adf07001bea23e0afb4033423ef268
Terry <luwangli@hotmail.com>
2021-06-23 07:52:12 +0800

    initial

--- scripts/source1.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system('tcpreplay -i h1-eth0 -p 50 /home/p4/P4/tutorials/ddosd-entropy/data/160000_0.1.pcap')
-
-
--- tools/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-make clean
-make Makefile
-./run.exe ../pcaps/s11-eth3_in.pcap
