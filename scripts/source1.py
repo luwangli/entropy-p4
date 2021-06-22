4cd4dacc11e6d583ad198b70b12fc77c4636face
Terry <luwangli@hotmail.com>
2021-06-22 19:25:43 +0800

    get data: entropy

--- scripts/source1.py
+++ scripts/source1.py
@@ -1,4 +1,4 @@
 import os
-os.system('tcpreplay -i h1-eth0 -p 50 /home/p4/P4/tutorials/ddosd-entropy/data/160000_0.04.pcap')
+os.system('tcpreplay -i h1-eth0 -p 50 /home/p4/P4/tutorials/ddosd-entropy/data/160000_0.1.pcap')
 
 
